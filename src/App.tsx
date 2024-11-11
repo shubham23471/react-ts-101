c6918193dc4489e824964344e2089a1ef69a94af
shubham <shubhamshakya61@gmail.com>
2024-11-11 18:39:16 +0530

    Using Props to pass data; passing item values dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import ListGroup from "./components/ListGroup";
 
 // we use this component like regular HTML tag
-function App(){
+function App() {
+  let items = ["New York", "San Franciso", "Tokyo", "London", "Paris"];
   // return <div><Message></Message></div>
-  return <div><ListGroup/> </div>
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />{" "}
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Franciso", "Tokyo", "London", "Paris"];
+//{ items : [], heading: string}
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+// parameter: props of Type Props
+function ListGroup({ items, heading }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {/* {items.length === 0 ? <p>No Item Found</p> : null}; */}
       {items.length === 0 && <p>No Item Found</p>}
 
@@ -32,45 +38,3 @@ function ListGroup() {
 }
 
 export default ListGroup;
-
-// import { MouseEvent } from "react";
-
-// function ListGroup() {
-//   let items = ["New York", "San Franciso", "Tokyo", "London", "Paris"];
-//   let selectedIndex = -1; // -1: No item is selected
-//   //   items = [];
-
-//   // we can also assing the value of a variable
-//   //   const message = items.length === 0 ? <p>No Item Found</p> : null;
-
-//   //Event handler
-//   const handleClick = (event: MouseEvent) => console.log(event);
-//   return (
-//     <>
-//       <h1>List</h1>
-//       {/* {items.length === 0 ? <p>No Item Found</p> : null}; */}
-//       {items.length === 0 && <p>No Item Found</p>}
-
-//       <ul className="list-group">
-//         {items.map((item, index) => (
-//           <li
-//             className={
-//               selectedIndex === index
-//                 ? "list-group-item active"
-//                 : "list-group-item"
-//             }
-//             key={item}
-//             // onClick={() => console.log(item, index)}
-//             onClick={handleClick}
-//           >
-//             {item}
-//           </li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// }
-
-// export default ListGroup;
-
-// ================================================
